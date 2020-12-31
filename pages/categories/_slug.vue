82c64082595b07f6f63b7f7b9711db5a4e00c2b2
Ryan <Harmon>
2020-12-31 15:09:58 -0700

    Hydrate store and navigation with Categories

---
+++ pages/categories/_slug.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container max-width-lg text-component padding-y-xl">
+        <h1>Category</h1>
+        <p>Products for a category</p>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss">
+</style>

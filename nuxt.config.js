909f22970c933e188de03de40bc31404b5f53a2a
Ryan <Harmon>
2021-03-16 13:34:52 -0600

    Change name

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
     ** Headers of the page
     */
     head: {
-        title: 'eagerclient',
+        title: 'nuxt-ecommerce',
         meta: [
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1' },
